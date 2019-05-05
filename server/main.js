e9f66c649d5677f821532a51059fda7b710da644
Yang <yangah9991@gmail.com>
2019-05-05 22:05:28 +0800

    Added Account packages

--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
+Meteor.startup(() => 
+{
   // code to run on server at startup
+  console.log("Our Meteor Server has Started");
 });
