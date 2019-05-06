d7dc59c61318efba44fa04744d99fb0a6b69a600
Yang <yangah9991@gmail.com>
2019-05-06 22:32:08 +0800

    setting up random generated poke Image on Map near us

--- server/main.js
+++ server/main.js
@@ -1,7 +1,41 @@
 import { Meteor } from 'meteor/meteor';
+import { Pokemon } from "../imports/collections/pokemon";
+var fs = Npm.require("fs");
 
 Meteor.startup(() => 
 {
   // code to run on server at startup
   console.log("Our Meteor Server has Started");
+});
+
+Meteor.methods({
+  "pokemon.add" : function(loc)
+  {
+    var user = this.userId;
+    if(!user)
+    {
+      console.log("User not signed in");
+      return;
+    }    
+
+    var range = 0.035;
+    var range1 = Math.random() > 0.5 ? range : -range;
+    var range2 = Math.random() > 0.5 ? range : -range;
+    var long = loc.longtitude;
+
+    long += Math.random() * (range1);
+
+    var lat = loc.latitude;
+    lat += Math.random() * (range2);
+
+    var iconPath = process.env.PWD + "./public";
+    var icons = fs.readdirSyn(iconPath);
+
+    var min = Math.ceil(0);
+    var max = Math.ceil(250);
+
+    var random = Math.floor(Math.random() * (max-min)) + min;
+
+    return Pokemon.insert({image : icons[random], longtitude : long, latitude : lat});
+  }
 });
